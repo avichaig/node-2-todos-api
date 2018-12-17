73fcc783eb9839ec2c9ab4bbf352b3f32cae6c9e
Avichai  Gez <avichaigez@gmail.com>
2018-12-17 11:56:35 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -103,8 +103,6 @@ app.post('/users', (req, res) => {
 
 });
 
-
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
@@ -117,6 +115,14 @@ app.post('/users/login', (req, res) => {
             res.header('x-auth', token).send(user);
         });
     }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
         res.status(400).send();
     });
 });
