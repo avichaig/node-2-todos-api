8f2cb4eae175c9a769d6fcbe4c174f0e28ac041a
Avichai  Gez <avichaigez@gmail.com>
2018-12-18 13:39:34 +0200

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,22 +12,28 @@ const users = [{
     password: 'userOnePass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userOneId, access: 'auth'}, 'Avigii').toString()
+        token: jwt.sign({ _id: userOneId, access: 'auth' }, 'Avigii').toString()
     }]
 }, {
     _id: userTwoId,
     email: 'avigi@test.com',
-    password: 'userTwoPass'
+    password: 'userTwoPass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({ _id: userTwoId, access: 'auth' }, 'Avigii').toString()
+    }]
 }];
 
 const todos = [{
     _id: new ObjectID(),
-    text: 'First test todo'
+    text: 'First test todo',
+    _creator: userOneId
 }, {
     _id: new ObjectID(),
     text: 'Second test todo',
     completed: true,
-    completedAt: 333
+    completedAt: 333,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
